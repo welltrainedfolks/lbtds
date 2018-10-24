a0f547b090ab543eaf63250bf50e14550cd6eef8
Vladimir Hodakov <vladimir@hodakov.me>
2018-10-24 19:39:40 +0400

    Fix function execution sequence

--- domains/proxies/v1/http_proxies.go
+++ domains/proxies/v1/http_proxies.go
@@ -108,13 +108,13 @@ func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	client := &http.Client{}
 	proxyRsp, err := client.Do(proxyReq)
-	defer proxyRsp.Body.Close()
 	if err != nil {
 		proxiesModuleLog.Error().Str("domain", domainToForward).Err(err).Msg("Can't connect to downstream")
 		http.Error(w, "Can't connect to downstream", responseCode)
 		proxiesModuleLog.Info().Str("remote", r.RemoteAddr).Str("domain", domainToForward).Int("code", responseCode).Int64("proxified bytes", proxifiedBytesCount).TimeDiff("request time (s)", time.Now(), start).Msg("Received HTTP request")
 		return
 	}
+	defer proxyRsp.Body.Close()
 
 	for header, values := range proxyRsp.Header {
 		for _, value := range values {
