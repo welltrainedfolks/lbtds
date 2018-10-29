a0a754752f9401b388eeb00a7de5c9a9692e8544
Vladimir Hodakov <vladimir@hodakov.me>
2018-10-29 18:15:26 +0400

    Add runtime.LockOSThread() to prevent disappearing from current shell
    This change should fix issue when LBTDS "detaches" from parent terminal session
    making Ctrl-C shutdown impossible.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 
 	"lab.wtfteam.pro/wtfteam/lbtds/context"
@@ -14,6 +15,11 @@ import (
 )
 
 func main() {
+	// Before any real work - lock to OS thread. We shouldn't leave it until
+	// shutdown
+	runtime.LockOSThread()
+
+	// And here is the rock'n'roll starts
 	c := context.NewContext()
 	c.Init()
 
